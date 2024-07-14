d04e721bb208892058b659ba23c7ac03444db375
Keloran <keloran@develbox.info>
2024-07-14 12:57:09 +0100

    this might work better

--- src/flags-gg__react-library.d.ts
+++ src/flags-gg__react-library.d.ts
@@ -1,1 +1,15 @@
-declare module '@flags-gg/react-library';
+declare module '@flags-gg/react-library' {
+  import { FC, ReactNode } from 'react';
+
+  interface FlagsProviderProps {
+    options: {
+      flagsURL: string;
+      projectId: string;
+      agentId: string;
+      environmentId: string;
+    };
+    children?: ReactNode;
+  }
+
+  export const FlagsProvider: FC<FlagsProviderProps>;
+}
